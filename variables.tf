1e5ea7825ff7d3f6855db17c04e35ac9b1e71abd
Ajay Chinthapalli <chinthapalli.ajay@gmail.com>
2022-06-30 13:58:09 -0700

    variables.tf

--- variables.tf
+++ variables.tf
@@ -33,3 +33,18 @@ variable "admin_username" {
   description = "Administrator user name for mysql"
   default     = "hashicorp"
 }
+
+variable "height" {
+  default     = "400"
+  description = "Image height in pixels."
+}
+
+variable "width" {
+  default     = "600"
+  description = "Image width in pixels."
+}
+
+variable "placeholder" {
+  default     = "placekitten.com"
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+}
